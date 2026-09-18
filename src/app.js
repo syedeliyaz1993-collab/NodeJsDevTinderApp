5fb6934ed5056d90db856fa977293844effb8d95
Syed Eliyaz <syedeliyaz1993@gmail.com>
2026-09-18 23:29:58 +0530

    Using HTTP Methods GET POST DELETE

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
 
+
+
+app.get("/user", (req, res) => {
+    res.send({
+        "fName" : "syed",
+        "lName": "Eshaan"
+    });
+});
+
+
+app.post("/user", (req, res) => {
+    res.send("Data saved successfully");
+});
+
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted successfully");
+});
+
+
+
+
 app.use('/express', (req, res) => {
     res.send("Hello World from Express!");
 });
