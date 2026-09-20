03fe7711fac5e6b5c23964d77c78c787cae5b7fa
Syed Eliyaz <syedeliyaz1993@gmail.com>
2026-09-20 23:06:20 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,44 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
+        required: true
     },
     age: {
         type: Number,
+        min: 18
     },
     gender: {
         type: String,
+        validate(value) {
+            const validGenders = ["male", "female", "other"];
+            if (!validGenders.includes(value)) {
+                throw new Error("Invalid gender value");
+            }
+        }
+    },
+    skills: {
+        type: [String],
     }
 
+}, 
+{
+    timestamps: true // Automatically adds createdAt and updatedAt fields
 });
 
 const User = mongoose.model("User", userSchema);
